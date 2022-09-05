cf76f01f963b07b265ac92fdc10e59a73bbcb648
mithridatem <mathieumithridate@adrar-formation.com>
2022-09-05 12:08:32 +0200

    ajout de la verification existence compte

--- createUser.php
+++ createUser.php
@@ -18,14 +18,21 @@
             $nom = cleanInput($_POST['nom_util']);
             $prenom = cleanInput($_POST['prenom_util']);
             $mail = cleanInput($_POST['mail_util']);
-            //version bcrypt
-            $password = password_hash(cleanInput($_POST['password_util']), PASSWORD_DEFAULT);
-            //version md5
-            //$password = md5($_POST['password_util']);
-            createUserV3($bdd,$nom, $prenom, $mail, $password);
-            $message = "le compte $nom à été ajouté en BDD";
-
-            //header('Location: ./createUser.php?nameCreate=');
+            //récupération du compte si il existe
+            $exist = showUserByMail($bdd, $mail);
+            //test si le compte existe
+            if(empty($exist)){
+                //version bcrypt
+                $password = password_hash(cleanInput($_POST['password_util']), PASSWORD_DEFAULT);
+                //fonction ajouter un utilisateur en BDD
+                createUserV3($bdd,$nom, $prenom, $mail, $password);
+                //message de confirmation
+                $message = "le compte $nom à été ajouté en BDD";
+            }
+            //test sinon le compte existe
+            else{
+                $message = "le compte existe déja";
+            }
         }
         //test si un ou plusieurs champs ne sont pas remplis
         else{
