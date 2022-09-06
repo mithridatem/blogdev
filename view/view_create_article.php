dadadd92df39eb04522d9809a215d911643a5e32
mithridatem <mathieumithridate@adrar-formation.com>
2022-09-06 09:36:47 +0200

    update view_create_article.php

--- view/view_create_article.php
+++ view/view_create_article.php
@@ -6,7 +6,5 @@
     <p><textarea name="contenu_art"></textarea></p>
     <p>Saisir date l'article</p>
     <p><input type="date" name="date_art"></p>
-    <p>Saisir le code :</p>
-    <p><input type="text" name="valeur_codeauto"></p>
     <p><input type="submit" value="ajouter" name="submit"></p>
 </form>
