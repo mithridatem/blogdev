45067d8f27c29c417e8e5bf57f96738aa5424c3c
mithridatem <mathieumithridate@adrar-formation.com>
2022-09-06 16:26:14 +0200

    update import file

--- utils/functions.php
+++ utils/functions.php
@@ -2,4 +2,8 @@
     function cleanInput($input){
         return htmlspecialchars(strip_tags(trim($input)));
     }
+    //fonction qui récupére le format du fichier (extension)
+    function get_file_extension($file){
+        return substr(strrchr($file,'.'),1);
+    }
 ?>
