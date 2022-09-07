14744e11c8cff2196e9ddabf118e3f423c469185
mithridatem <mathieumithridate@adrar-formation.com>
2022-09-07 15:13:16 +0200

    update structure projet

--- view/view_navbar.php
+++ view/view_navbar.php
@@ -2,11 +2,10 @@
     <!--Menu navbar -->
     <nav>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="./showAllArticle">Home</a></li>
+            <li><a href="./createArticle">Ajouter article</a></li>
+            <li><a href="./createArticleCode">Ajouter article code</a></li>
+            <li><a href="./createUser">Ajouter utilisateur</a></li>
         </ul>
     </nav>
 
